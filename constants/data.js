43248de00d740b1af46a3b43143201efca4460bd
Mohamed Bilal <66405078+mbilal51@users.noreply.github.com>
2025-01-18 17:47:56 +0530

    Add files via upload

---
+++ constants/data.js
@@ -0,0 +1,106 @@
+const userData = {
+  githubUsername: "mbilal51",
+  name: "Mohamed Bilal",
+  designation: "Product Specialist",
+  avatarUrl: "/avatar.png",
+  email: "mdbillu.51@gmail.com",
+  phone: "+91 9886882024",
+  address: "Bangalore, India",
+  projects: [
+    {
+      title: "Random Clicks",
+      link: "https://randomclicks.bilal.in",
+      imgUrl: "/randomclicks.png",
+    },
+    {
+      title: "NextJS Commerce Demo",
+      link: "https://commerce.nocturnalstudio.in",
+      imgUrl: "/commercedemo.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://bilal.in",
+      imgUrl: "/portfoliosite.png",
+    },
+    
+  ],
+  about: {
+    title:
+      "I am a Product Specialist at Limelight Networks with extensive experience across multiple domains",
+    description: [
+      `I have been coding in HTML, CSS, JS, ReactJS, NextJS, Tailwind, Python, Bootstrap etc`,
+      `Specialities - Cloud Security, Media/Streaming, Content Delivery(CDN), Web Development(HTML/CSS/JS), EdgeJS, Edge Compute` ,
+    ],
+    currentProject: "Random Clicks",
+    currentProjectUrl: "https://randomclicks.bilal.in",
+  },
+  experience: [
+    {
+      title: "Solutions Engineer",
+      company: "Akamai Technologies",
+      imgUrl: "/akamai-logo.png",
+      year: "Aug 2022",
+      companyLink: "https://akamai.com",
+      desc: "Media/Streaming, Cloud Security, Web Performance, Edge Compute",
+    },
+    {
+      title: "Product Specialist",
+      company: "Limelight Networks",
+      imgUrl: "/limelight-logo.png",
+      year: "2021 - 2022",
+      companyLink: "https://limelight.com",
+      desc: "Layer0 - EdgeJS, EdgeXtend",
+    },
+    {
+      title: "Solutions Engineer",
+      company: "Akamai Technologies",
+      imgUrl: "/akamai-logo.png",
+      year: "2018 - 2021",
+      companyLink: "https://akamai.com",
+      desc: "Media/Streaming, Cloud Security, Web Performance, Edge Compute",
+    },
+    {
+      title: "Solutions Consultant",
+      company: "Hewlett Packard Enterprise",
+      imgUrl: "/hpe-logo.png",
+      year: "2016 - 2018",
+      companyLink: "https://www.hpe.com",
+      desc: "",
+    },
+    {
+      title: "Infrastructure Engineer",
+      company: "Dell Technologies",
+      imgUrl: "/dell-technologies-logo.png",
+      year: "2015 - 2016",
+      companyLink: "https://dell.com",
+      desc: "",
+    },
+    {
+      title: "Infrastructure Engineer",
+      company: "Hewlett Packard Inc",
+      imgUrl: "/hpe-logo.png",
+      year: "2013 - 2014",
+      companyLink: "https://www.hpe.com",
+      desc: "",
+    },
+    {
+      title: "Bachelor of Engineering, BE",
+      company: "Visvesvaraya Technological University",
+      imgUrl: "/vtu-logo.png",
+      year: "2013",
+      companyLink: "https://vtu.ac.in",
+      desc: "Computer Science",
+    },
+  ],
+  resumeUrl:
+    "",
+  socialLinks: {
+    instagram: "https://instagram.com",
+    twitter: "https://twitter.com",
+    linkedin: "https://www.linkedin.com/in/mohamed-bilal-521b59219",
+    github: "https://github.com/mbilal51",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+//     colors: {
+// 'black1': ' #191919',
+// 'black2': ' #000000 ',
+//     },
+    extend: {},
+  },
+  plugins: [],
+}
